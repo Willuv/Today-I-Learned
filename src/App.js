27fb63f959bc2defc52ba7af2d782d2c5ff92d9a
William Uvlin <Will.Uvlin@gmail.com>
2024-07-21 21:42:35 -0400

    begin transforming html webpage into javascript

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+// connect to the style.css file
+import './style.css'
+
 function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <header className="header">
+      <div className="logo">
+        {/* main logo of the website */}
+        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo"/>
+        {/* main header */}
+        <h1>Today I Learned</h1>
+      </div>
+    <button className="btn btn-large btn-open">Share a fact</button>
+  </header>
+  );
 }
 
 export default App;
